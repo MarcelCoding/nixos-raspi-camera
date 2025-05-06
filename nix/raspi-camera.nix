87e8f78ae6e5ba1345beccfc3266711372350b54
nviets <nathan.g.viets@gmail.com>
2025-05-06 18:31:45 -0500

    disable libav
    (cherry picked from commit bb1492a8e3008526d083816f2e5401200d1ec0d4)

--- nix/raspi-camera.nix
+++ nix/raspi-camera.nix
@@ -1,4 +1,4 @@
-{pkgs, config, lib, ...}:
+{ pkgs, config, lib, ... }:
 let
   cfg = config.hardware.raspi-camera;
   user = "rpicam-rtsp-server";
@@ -6,59 +6,59 @@ let
 in
 with lib;
 {
-  options = {
-    hardware.raspi-camera = {
-    };
-  };
-
+  #  options = {
+  #    hardware.raspi-camera = {
+  #    };
+  #  };
+  #
   config = with campkgs; {
-    services.udev = {
-      extraRules = ''
-        SUBSYSTEM=="dma_heap", GROUP="video", MODE="0660"
-      '';
-    };
+    #    services.udev = {
+    #      extraRules = ''
+    #        SUBSYSTEM=="dma_heap", GROUP="video", MODE="0660"
+    #      '';
+    #    };
 
-    systemd.services.dtmerger = {
-      script = ''
-        ${dtmerger}/bin/dtmerger.sh
-      '';
-      wantedBy = [ "multi-user.target" ];
-      serviceConfig = {
-        Type = "oneshot";
-        RemainAfterExit = true;
-      };
-    };
+    #    systemd.services.dtmerger = {
+    #      script = ''
+    #        ${dtmerger}/bin/dtmerger.sh
+    #      '';
+    #      wantedBy = [ "multi-user.target" ];
+    #      serviceConfig = {
+    #        Type = "oneshot";
+    #        RemainAfterExit = true;
+    #      };
+    #    };
 
-    users.groups."${user}" = {};
-
-    users.extraUsers."${user}" = {
-      group = user;
-      isSystemUser = true;
-      home = "/var/spool/${user}";
-      createHome = true;
-      extraGroups = [
-        "video" "plugdev"
-      ];
-    };
-
-    systemd.services.rpicam-rtsp-server = {
-      script = with pkgs; ''
-        ${rpicam-apps}/bin/rpicam-vid -t 0 --inline -o - \
-          --hflip --vflip --width 320 --height 240 \
-          2>/dev/null \
-        | ${vlc}/bin/cvlc stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8554/stream1}' \
-          :demux=h264 \
-        ;
-      '';
-      after = [ "dtmerger.service" ];
-      wantedBy = [ "multi-user.target" ];
-      serviceConfig = {
-        Restart = "always";
-        User = user;
-        Group = user;
-      };
-    };
-
+    #    users.groups."${user}" = {};
+    #
+    #    users.extraUsers."${user}" = {
+    #      group = user;
+    #      isSystemUser = true;
+    #      home = "/var/spool/${user}";
+    #      createHome = true;
+    #      extraGroups = [
+    #        "video" "plugdev"
+    #      ];
+    #    };
+    #
+    #    systemd.services.rpicam-rtsp-server = {
+    #      script = with pkgs; ''
+    #        ${rpicam-apps}/bin/rpicam-vid -t 0 --inline -o - \
+    #          --hflip --vflip --width 320 --height 240 \
+    #          2>/dev/null \
+    #        | ${vlc}/bin/cvlc stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8554/stream1}' \
+    #          :demux=h264 \
+    #        ;
+    #      '';
+    #      after = [ "dtmerger.service" ];
+    #      wantedBy = [ "multi-user.target" ];
+    #      serviceConfig = {
+    #        Restart = "always";
+    #        User = user;
+    #        Group = user;
+    #      };
+    #    };
+    #
     nixpkgs.config.permittedInsecurePackages = [
       "libav-11.12"
     ];
